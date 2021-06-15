b24ce73b34970b1b0caafd93d2963a34e6772621
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-06-15 12:14:13 +0800

    added game loop

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 //* Data
-let score = 3;
+let score = 1;
 
+let lightDuration = 650;
 let noteInterval = 1000;
-let lightDuration = 500;
 
 const userChoice = [];
 const randomized = [];
 
 //* Mode Options
-const majorPentatonic = ["red", "orange", "yellow", "turquoise", "blue"];
-const majorScale = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "turquoise",
-  "blue",
-  "purple",
-];
+// const majorScale = {
+//   colours: [
+//     "red",
+//     "orange",
+//     "yellow",
+//     "green",
+//     "turquoise",
+//     "blue",
+//     "purple",
+//   ],
+// };
+const majorPentatonic = {
+  colours: ["red", "orange", "yellow", "turquoise", "blue"],
+  // notes: ["src =" /Sounds/RedC-4.mp3"", ]
+};
 
-//* Functions
+//! Functions
+//*==========GENERATES RANDOM PATTERN BASED ON CURRENT SCORE AND THE MODE SENDS IT OVER TO NEW ARRAY
+const generator = (score, mode) => {
+  for (let i = 1; i <= score; i++) {
+    let random = Math.floor(Math.random() * mode.length);
+    randomized.push(mode[random]);
+  }
+  randomizer();
+};
+
+//*==========DISPLAYS THE COLOURS ON THE SCREEN BASED ON GENERATOR ARRAY [randomized]
 const randomizer = () => {
   randomized.forEach((item, index) => {
     setTimeout(() => {
-      // console.log(randomized[index]);
+      console.log(randomized[index]);
       $(`#${item}`).css("background", randomized[index]);
       setTimeout(() => {
         $(`#${item}`).css("background", "");
@@ -33,78 +48,107 @@ const randomizer = () => {
   });
 };
 
-const generator = (score, mode) => {
-  for (let i = 1; i <= score; i++) {
-    let random = Math.floor(Math.random() * mode.length);
-    randomized.push(mode[random]);
-    console.log(random);
-  }
-  randomizer();
-};
-
+//*==========TAKES USER INPUT AND STORES ITg IN NEW ARRAY [userChoice]
 const user = () => {
   $(".playBox").on("click", (e) => {
-    console.log("clicked", e.target.id);
     userChoice.push(e.target.id);
+    console.log("clicked", e.target.id);
     compare(userChoice, randomized);
   });
 };
 
+//*==========COMPARES ARRAYS OF USER AND THE COMPUTER [randomized] [userChoice]
 const compare = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
-        score = 1;
-        $("#score").text(`Current score: ${score}`);
-        return score;
+        reset();
+        return;
       } else {
-        score += 1;
-        $("#score").text(`Current score: ${score}`);
-        return score;
+        nextLevel();
+        return;
       }
     }
   }
+};
+
+const reset = () => {
+  score = 1;
+  $("#score").text(`Current score: ${score}`);
+  alert("Wrong patter, going back to level 1");
+  userChoice.splice(0, userChoice.length);
+  randomized.splice(0, randomized.length);
+  generator(score, majorPentatonic.colours);
+  console.log(userChoice);
+  console.log(randomized);
+  return;
+};
+
+const nextLevel = () => {
+  score += 1;
+  $("#score").text(`Current score: ${score}`);
+  userChoice.splice(0, userChoice.length);
+  randomized.splice(0, randomized.length);
+  generator(score, majorPentatonic.colours);
+  console.log(userChoice);
+  console.log(randomized);
+  return;
 };
 
 //! BUTTONS!
 const buttons = () => {
   $("#score").text(`Current score: ${score}`);
 
-  $("#resetOne").on("click", () => {
-    score = 1;
-    $(".main").toggle();
-    $("#gameOne").toggle();
+  $(".next").on("click", () => {
+    // generator(score, majorPentatonic.colours);
   });
 
-  $("#resetTwo").on("click", () => {
-    score = 1;
-    $(".main").toggle();
-    $("#gameTwo").toggle();
-  });
-
-  //* Mode One
+  //? Mode One
   $("#gameOne").toggle();
   $("#modeOne").on("click", () => {
     $("#gameOne").toggle();
     $(".main").toggle();
+    // generator(score, majorPentatonic.colours);
   });
 
-  //* Mode Two
-  $("#gameTwo").toggle();
-  $("#modeTwo").on("click", () => {
-    $("#gameTwo").toggle();
+  $("#resetOne").on("click", () => {
+    $(".main").toggle();
+    $("#gameOne").toggle();
+    score = 1;
+    $("#score").text(`Current score: ${score}`);
+  });
+
+  //? Mode Two
+  // $("#gameTwo").toggle();
+  // $("#modeTwo").on("click", () => {
+  //   $("#gameTwo").toggle();
+  //   $(".main").toggle();
+  // });
+
+  // $("#resetTwo").on("click", () => {
+  //   $(".main").toggle();
+  //   $("#gameTwo").toggle();
+  //   score = 1;
+  // });
+
+  //? Settings
+  $("#settings").toggle();
+  $("#set").on("click", () => {
+    $("#settings").toggle();
+    $(".main").toggle();
+  });
+  $("#back").on("click", () => {
+    $("#settings").toggle();
     $(".main").toggle();
   });
 };
-$(() => {
-  buttons();
-  user();
-  generator(score, majorPentatonic);
-  // gameUp();
-  // compare(randomized, userChoice);
-  $("#score").text(`Current score: ${score}`);
-});
 
-//? ADD ON CLICK TO CHANGE THE MODES INSIDE THE FUNCTION
-//? SETTING TO BE ABLE CHANGE THE BLINKING TIME AND NOTE DISTANCE
-//? HOW TO LOOP THE GAME!?
+$("#score").text(`Current score: ${score}`);
+
+//*===================MAIN========================
+const main = () => {
+  // buttons();
+  generator(score, majorPentatonic.colours); // calls randomizer - blinks the lights
+  user(); // calls the compare functions compares calls either reset or next level based on the outcome
+};
+$(main);
