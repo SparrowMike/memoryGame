00fd2b8e1d93b05d51a4bfc088773d8ae8a10b41
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-06-20 19:29:52 +0800

    jQuery generating modes, and other major improvments

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
-//*=====================DATA======================
-let audioVolume = 0.8;
-let lightDuration = 300;
-let noteInterval = 600;
-let audioSpeed = 1.5;
-let score = 1;
+//!======================MODES========================
+const pentatonic = {
+  id: ["C", "D", "E", "G", "A"],
+  colour: ["red", "orange", "yellow", "turquoise", "blue"],
+  rotate: ["270deg", "345deg", "60deg", "120deg", "195deg"],
+  hoverBackground: ["#e31918", "#ff7b00", "#ffea00", "#80ffdb", "#072ac8"],
+  shadow: ["lightpink", "lightsalmon", "#ffffb7", "#caf0f8", "#48cae4"],
+  audio: [
+    "/Sounds/C4.mp3",
+    "/Sounds/D4.mp3",
+    "/Sounds/E4.mp3",
+    "/Sounds/G4.mp3",
+    "/Sounds/A5.mp3",
+  ],
+};
+
+const major = {
+  id: ["1", "2", "3", "4", "5", "6", "7"],
+  colour: ["red", "orange", "green", "yellow", "turquoise", "blue", "purple"],
+  rotate: [
+    "270deg",
+    "321.43deg",
+    "12.86deg",
+    "64.29deg",
+    "115.72deg",
+    "167.15deg",
+    "218.58deg",
+  ],
+  hoverBackground: [
+    "#e31918",
+    "#ff7b00",
+    "#38b000",
+    "#ffea00",
+    "#80ffdb",
+    "#072ac8",
+    "#7e0afa",
+  ],
+  shadow: [
+    "lightpink",
+    "lightsalmon",
+    "TBC",
+    "#ffffb7",
+    "#caf0f8",
+    "#48cae4",
+    "TBC",
+  ],
+  audio: [
+    "/Sounds/C4.mp3",
+    "/Sounds/D4.mp3",
+    "/Sounds/E4.mp3",
+    "/Sounds/F4.mp3",
+    "/Sounds/G4.mp3",
+    "/Sounds/A5.mp3",
+    "/Sounds/B5.mp3",
+  ],
+};
 
 //*====================ARRAYS=====================
-const userChoice = [];
-const computerChoice = [];
 const motivation = [
   "Great Job!",
   "Well Played!",
@@ -19,31 +67,68 @@ const motivation = [
   "Mesmerizing!",
   "Astonishing!",
 ];
-const pentatonic = ["red", "orange", "yellow", "turquoise", "blue"];
 
-//! Functions
-//*==========GENERATES RANDOM PATTERN=============
+const userChoice = [];
+const computerChoice = [];
+
+//*=====================DATA======================
+let audioVolume = 0.8;
+let lightDuration = 525;
+let noteInterval = 600;
+let audioSpeed = 1;
+let score = 1;
+
+let mode = pentatonic;
+
+const gameWindow = (mode) => {
+  for (let i = 0; i < mode.id.length; i++) {
+    $audio = $("<audio>")
+      .attr("class", `${mode.id[i]}-audio`)
+      .attr("id", `${mode.id[i]}-audio`)
+      .attr("src", mode.audio[i])
+      .addClass("audio");
+    $(`#${mode.id[i]}`).append($audio);
+
+    $(`#${mode.id[i]}:nth-child(${[i + 1]})`)
+      .css({
+        transform: `rotate(${mode.rotate[i]}) translateX(125px)`,
+      })
+      .css("background-color", mode.colour[i]);
+
+    $(`#${mode.id[i]}`)
+      .on("mouseenter", () => {
+        $(`#${mode.id[i]}:nth-child(${[i + 1]})`).css({
+          "box-shadow": ` 0 0.5em 0.5em -0.4em ${mode.hoverBackground[i]}`,
+        });
+      })
+      .on("mouseleave", () => {
+        $(`#${mode.id[i]}:nth-child(${[i + 1]})`).css("box-shadow", "none");
+      });
+  }
+};
+
+//!=================MODE PICKED========================
+//*============GENERATES RANDOM PATTERN==============
 const generator = (score, mode) => {
   for (let i = 1; i <= score; i++) {
-    let random = Math.floor(Math.random() * mode.length);
-    computerChoice.push(mode[random]);
+    let random = Math.floor(Math.random() * mode.id.length);
+    computerChoice.push(mode.id[random]);
   }
   generatorAudioVisual();
 };
 
+// //! Functions
 //*=============COMPUTER OUTPUT AV================
 const generatorAudioVisual = () => {
   computerChoice.forEach((item, index) => {
-    let time = setTimeout(() => {
+    setTimeout(() => {
       //*======================AUDIO======================
-      if (item) {
-        document.querySelector(`#${item}-audio`).playbackRate = audioSpeed;
-        document.querySelector(`#${item}-audio`).volume = audioVolume;
-      }
-      document.querySelector(`#${item}-audio`).pause();
-      document.querySelector(`#${item}-audio`).currentTime = 0;
+      $(`#${item}-audio`).prop(
+        { playbackRate: audioSpeed },
+        { volume: audioVolume }
+      );
+      $(`#${item}-audio`).trigger("pause").prop("currentTime", 0);
       $(`#${item}-audio`).get(0).play();
-
       //*======================VISUAL======================
       $(`#${item}`).css({
         opacity: 1,
@@ -51,10 +136,13 @@ const generatorAudioVisual = () => {
         transition: "background-color 0.3s ease",
         "box-shadow": "0 0.5em 0.5em -0.4em #dcd9cf",
       });
-      console.log(`Computer choice: #${item}`);
-
       setTimeout(() => {
-        $(`#${item}`).removeAttr("style");
+        $(`#${item}`).css({
+          opacity: "",
+          background: item,
+          transition: "background-color 0.3s ease",
+          "box-shadow": "none",
+        });
       }, lightDuration);
     }, noteInterval * index);
   });
@@ -65,12 +153,12 @@ const user = () => {
   $(".game").on("click", (e) => {
     if (e.target.id) {
       userChoice.push(e.target.id);
-      console.log("User clicked", e.target.id);
-      document.querySelector(`#${e.target.id}-audio`).volume = audioVolume;
-      document.querySelector(`#${e.target.id}-audio`).playbackRate = audioSpeed;
-      document.querySelector(`#${e.target.id}-audio`).pause();
-      document.querySelector(`#${e.target.id}-audio`).currentTime = 0;
-      $(`#${e.target.id}-audio`).get(0).play();
+      $(`.${e.target.id}-audio`).prop(
+        { playbackRate: audioSpeed },
+        { volume: audioVolume }
+      );
+      $(`.${e.target.id}-audio`).trigger("pause").prop("currentTime", 0);
+      $(`.${e.target.id}-audio`).get(0).play();
       if (userChoice.length === computerChoice.length) {
         compare(userChoice, computerChoice);
       }
@@ -90,18 +178,13 @@ const compare = (arr1, arr2) => {
   }
 };
 
-//*==============RESET ARRAYS=====================
-const resetArr = () => {
-  $("#score").text(`Current score: ${score}`);
-  userChoice.splice(0, userChoice.length);
-  computerChoice.splice(0, computerChoice.length);
-};
-
-//*================GAME OVER======================
-const reset = () => {
-  score = 1;
+//*=================LEVEL UP======================
+const nextLevel = () => {
+  score++;
   resetArr();
-  $("#popUp").dialog("open");
+  setTimeout(() => {
+    generator(score, mode);
+  }, 800);
   return;
 };
 
@@ -120,13 +203,18 @@ const keepUp = () => {
   }, 750);
 };
 
-//*=================LEVEL UP======================
-const nextLevel = () => {
-  score++;
+//*==============RESET ARRAYS=====================
+const resetArr = () => {
+  $("#score").text(`Current score: ${score}`);
+  userChoice.splice(0, userChoice.length);
+  computerChoice.splice(0, computerChoice.length);
+};
+
+//*================GAME OVER======================
+const reset = () => {
+  score = 1;
   resetArr();
-  setTimeout(() => {
-    generator(score, pentatonic);
-  }, 800);
+  $("#popUp").dialog("open");
   return;
 };
 
@@ -136,45 +224,51 @@ const buttons = () => {
   $("#popUp").dialog({ autoOpen: false }, { title: "Game Over!" });
   $("#modes").dialog({ autoOpen: false }, { title: "Modes" });
 
-  //? START THE GAME
-  $("#gameOne").toggle();
-  $("#modeOne").on("click", () => {
-    $("#gameOne").toggle();
+  //?===============START THE GAME================
+  $("#gameWindowOne, #gameWindowTwo, #gameButtons").toggle();
+  $("#gameOn").on("click", () => {
+    if (mode === pentatonic) {
+      $("#gameWindowOne").toggle();
+    } else {
+      $("#gameWindowTwo").toggle();
+    }
+    $("#gameButtons").toggle();
     $(".main").toggle();
+    gameWindow(mode);
     setTimeout(() => {
-      generator(score, pentatonic);
-    }, 100);
+      generator(score, mode);
+    }, 300);
+  });
+
+  //?==============REPEAT THE PATTERN=============
+  $(".playAgain").on("click", () => {
+    generatorAudioVisual();
   });
 
   //?================RESET THE GAME===============
   $(".reset").on("click", () => {
     score = 1;
-    document.querySelector(".audio").pause();
-    document.querySelector(".audio").currentTime = 0;
+    $(".audio").trigger("pause");
+    $(".audio").prop("currentTime", 0);
     resetArr();
     $("#popUp").dialog("close");
-    generator(score, pentatonic);
+    generator(score, mode);
   });
 
   //?=================BACK TO MAIN================
   $(".back").on("click", () => {
     $(".main").toggle();
-    $("#gameOne").toggle();
+    if (mode === pentatonic) {
+      $("#gameWindowOne").toggle();
+    } else {
+      $("#gameWindowTwo").toggle();
+    }
+    $("#gameButtons").toggle();
     resetArr();
-    document.querySelector(".audio").pause();
-    document.querySelector(".audio").currentTime = 0;
+    $(".audio").trigger("pause");
+    $(".audio").prop("currentTime", 0);
     score = 1;
     $("#score").text(`Current score: ${score}`);
-  });
-
-  //?=================PANIC!!!!===================
-  $(".panic").on("click", () => {
-    location.reload();
-  });
-
-  //?============REPEAT THE PATTERN===============
-  $(".playAgain").on("click", () => {
-    generatorAudioVisual();
   });
 
   //?================SETTINGS=====================
@@ -193,11 +287,17 @@ const buttons = () => {
 
   //?==============COLLECT VALUE===================
   const settingsValue = () => {
-    lightDuration = $("#lightDuration").val();
     noteInterval = $("#noteInterval").val();
     audioSpeed = $("#audioSpeed").val();
     audioVolume = $("#volume").val();
     score = $("#scoreInput").val();
+
+    if ($("#mode").val() === "pentatonic") {
+      mode = pentatonic;
+    } else if ($("#mode").val() === "major") {
+      mode = major;
+    }
+    lightDuration = noteInterval - 75;
   };
 
   //?==================ABOUT=======================
@@ -207,9 +307,33 @@ const buttons = () => {
   });
 };
 
+//?=================PANIC!!!!===================
+$(".panic").on("click", () => {
+  location.reload();
+});
+
 //*===================MAIN========================
 const main = () => {
   buttons();
   user();
 };
 $(main);
+
+//!=====================================================================================================================================================================
+
+// TODO MUST HAVE
+// reset the setTimeout - affecting audio and not allowing the reset to execute - walkaround location.reload()
+// Reset timeOut on repeat button !!!
+// on/off for lights and sound?
+//! RESET THE CSS AFTER BACK OR RESET?
+
+// TODO SHOULD HAVE
+
+// TODO NICE TO HAVE
+// Additional explanation
+//* SETTINGS - print out current value of light duration, speed etc
+//* ability to change the instrument
+// Notes left countdown-maybe
+// CHORDS?!
+// Ability to use the keyboard
+// BPM!!!
